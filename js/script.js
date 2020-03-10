594330e48ff6d43f3543e1c6008fb17f596c8afa
solsworks <solwpaul1@gmail.com>
2020-03-10 17:12:42 -0400

    titles

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
 /*****************
 
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
+ChartYourMusic
 
 ******************/
