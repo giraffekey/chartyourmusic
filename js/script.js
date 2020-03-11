b32a60273eef45fd9ed0a7b8959e966bd164130f
GiraffeKey <giraffekey@tutanota.com>
2020-03-10 17:48:19 -0700

    Test chart and chart list

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@
 ChartYourMusic
 
 ******************/
+
+function resizeTiles() {
+    $('.tile').each((i, tile) => {
+        tile.style.height = tile.offsetWidth+'px';
+    });
+}
+
+resizeTiles();
+window.onresize = resizeTiles;
