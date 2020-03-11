73dd947409e2f3df753f364511570ee48da15678
GiraffeKey <giraffekey@tutanota.com>
2020-03-11 12:33:22 -0700

    Restyle everything

--- js/script.js
+++ js/script.js
@@ -4,6 +4,14 @@ ChartYourMusic
 
 ******************/
 
+function optionsArrow() {
+    let arrow = $('#optionsArrow');
+    if(arrow.html()==='&#9660;')
+      arrow.html('&#9650;');
+    else
+      arrow.html('&#9660;');
+}
+
 function resizeTiles() {
     $('.tile').each((i, tile) => {
         tile.style.height = tile.borderWidth+'px';
