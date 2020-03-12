31533742c1a1cb5c408ce0648192c512c0c06320
GiraffeKey <giraffekey@tutanota.com>
2020-03-12 15:13:42 -0700

    Dropping images on tiles

--- js/script.js
+++ js/script.js
@@ -5,19 +5,11 @@ ChartYourMusic
 ******************/
 
 function optionsArrow() {
-<<<<<<< HEAD
-    let arrow = $('#optionsArrow');
-    if(arrow.html()==='Options ▼')
-        arrow.html('Options ▲');
-    else
-        arrow.html('Options ▼');
-=======
   let arrow = $('#optionsArrow');
-  if (arrow.html() === '▼')
-    arrow.html('▲');
+  if (arrow.html() === 'Options ▼')
+    arrow.html('Options ▲');
   else
-    arrow.html('▼');
->>>>>>> 05dadee51e358a1acd9318e3dd1a785e38612d40
+    arrow.html('Options ▼');
 }
 
 function resize() {
@@ -49,45 +41,39 @@ function checkEnter() {
 }
 
 function getAlbums() {
-  let artist = $('#artist').val();
-  let album = $('#album').val();
-  $('#results').html('');
-  fetch(`https://musicbrainz.org/ws/2/release?query=${album}&limit=5?inc=artist-credit&fmt=json`,
+    let artist = $('#artist').val();
+    let album = $('#album').val();
+    $('#results').html('');
+    fetch(`https://musicbrainz.org/ws/2/release?query=${album}&limit=5?inc=artist-credit&fmt=json`,
     (resp) => {
       const releases = JSON.parse(resp).releases;
       for (let i = 0; i < releases.length; i++) {
-        console.log(releases[i]['artist-credit'][0].name);
         fetch('https://coverartarchive.org/release/' + releases[i]['id'],
           (resp) => {
             const images = JSON.parse(resp).images;
             for (let i = 0; i < images.length; i++) {
-              $('#results').append(`
-                        <img class="ui-draggable" src="${images[i]['image']}">
-                    `)
-              $(".ui-draggable").draggable({
-                containment: "document",
-                scroll: false,
-                stack: "img.ui-draggable"
-              })
-              // .mousedown(function () {
-              let stack = $(".ui-draggable").draggable("option", "stack");
-              $(".ui-draggable").draggable("option", "stack", "img.ui-draggable");
-              // });
+                let img = document.createElement('img');
+                img.src = images[i]['image'];
+                $(img).draggable({
+                    appendTo: 'body',
+                    zIndex: 10,
+                    helper: 'clone',
+                    start: (e, ui) => {
+                        $(ui.helper).css({'width': $('#results').width() / 2});
+                    }
+                });
+                $('#results').append(img);
             }
-          });
+        });
       }
     });
 }
 
-<<<<<<< HEAD
-function getAlbumsEnter(e) {
-    if(e.key === 'Enter') {
-        getAlbums();
+$('.tile').droppable({
+    accept: '.ui-draggable',
+    drop: (e, ui) => {
+        e.target.src = $(ui.draggable).attr('src');
     }
-}
+});
 
-$('#artist').keypress(getAlbumsEnter);
-$('#album').keypress(getAlbumsEnter);
-=======
 // https://stackoverflow.com/questions/5941631/compile-save-export-html-as-a-png-image-using-jquery
->>>>>>> 05dadee51e358a1acd9318e3dd1a785e38612d40
